aa13ace558ae3ca4be408a7dd2ac217d8b6ff3f4
rrrago1 <ragogarcia1@gmail.com>
2021-03-23 19:26:46 -0500

    shared module

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { AdvertisementComponent } from './advertisement/advertisement.component';
 
 @NgModule({
     declarations: [
